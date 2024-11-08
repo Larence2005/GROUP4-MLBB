5c36d0ae5074ba713215c76803aec504260e740a
John Larence Lusaya <150530035+Larence2005@users.noreply.github.com>
2024-11-08 21:03:18 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -92,6 +92,8 @@ elif st.session_state.page_selection == 'dataset':
     st.header("Dataset")
     st.write("Here is a preview of the dataset used in this analysis.")
     st.write(df)
+    missing_count = df.isnull().sum()
+    missing_count
     print(df.info())
     df.describe()
 
