fa032d930d49192f06c63422a1ee2ac10bb37cc6
John Larence Lusaya <150530035+Larence2005@users.noreply.github.com>
2024-11-08 19:31:54 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,14 @@
-import streamlit as st
 import matplotlib.pyplot as plt
 import numpy as np
 import networkx as nx
 import pandas as pd
 import seaborn as sns
 import altair as alt
-import joblib
 from wordcloud import WordCloud
 from google.colab import files
 from mpl_toolkits.mplot3d import Axes3D
 from pandas.plotting import scatter_matrix
+from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from sklearn.naive_bayes import MultinomialNB
@@ -20,6 +19,7 @@ from sklearn.cluster import KMeans
 from sklearn.semi_supervised import LabelPropagation
 from sklearn.impute import SimpleImputer
 from sklearn.feature_extraction.text import TfidfVectorizer
+
 
 df = pd.read_csv("Mlbb_Heroes.csv")
 df
