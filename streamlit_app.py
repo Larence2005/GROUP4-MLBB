5a3cc01d3def55a14df92ce55433670ad8f983c8
John Larence Lusaya <150530035+Larence2005@users.noreply.github.com>
2024-11-14 18:10:28 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,8 +8,9 @@ import altair as alt
 from wordcloud import WordCloud
 from mpl_toolkits.mplot3d import Axes3D
 from pandas.plotting import scatter_matrix
-from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, classification_report
